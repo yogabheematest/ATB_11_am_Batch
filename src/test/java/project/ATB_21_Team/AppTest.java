9661650a1010198b9fc87f88a8cdfa2bc59b8255
yogabheematest <yogabheematest@gmail.com>
2023-02-21 12:11:31 +0530

    project is commited by yoganand

---
+++ src/test/java/project/ATB_21_Team/AppTest.java
@@ -0,0 +1,20 @@
+package project.ATB_21_Team;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
